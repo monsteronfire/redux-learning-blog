95a2272a712a5a1146dc4bbedcbbfc8982b3c31a
Shaii Ong <monsteronfire@gmail.com>
2017-08-31 01:44:45 +0800

    Add scaffolding for posts new component

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostsNew extends React.Component {
+  render() {
+    return (
+      <div className='posts-new'>
+        Posts new
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
