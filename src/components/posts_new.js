e3a8c773cc63a8263f8fe9bb98be8326671bbdcc
Shaii Ong <monsteronfire@gmail.com>
2017-09-11 09:46:18 +0800

    Add initial posts new form elements

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends React.Component {
   render() {
     return (
-      <div className='posts-new'>
-        Posts new
-      </div>
+      <form className='posts-new'>
+        <Field
+          name='title'
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
