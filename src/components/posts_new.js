faf2222898b5bea1cc753ecdaf5739bcc093ff8b
Shaii Ong <monsteronfire@gmail.com>
2017-09-13 23:48:53 +0800

    Add cancel button

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends React.Component {
   renderField(field) {
@@ -45,6 +46,7 @@ class PostsNew extends React.Component {
           component={this.renderField}
         />
         <button type='submit' className='btn btn-primary'>Submit</button>
+        <Link to='/' className='btn btn-danger'>Cancel</Link>
       </form>
     );
   }
