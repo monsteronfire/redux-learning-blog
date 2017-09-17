9f9fb7b5c906d6e301bdf415a6a9986b38204030
Shaii Ong <monsteronfire@gmail.com>
2017-09-17 22:37:45 +0800

    Get single post in component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import connect from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends React.Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
     return (
       <div>
         Posts Show
       </div>
     )
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(null, { fetchPost })(PostsShow);
